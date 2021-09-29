a7f46a1f52fa16556a15165fe7aa69e88fce4ff1
KaKaoPark <49482420+KaKaoPark857@users.noreply.github.com>
2021-09-29 12:15:37 +0900

    Main Source code Folder
    Main source Code

---
+++ SmartCityProject/SendMessage/json_open.py
@@ -0,0 +1,11 @@
+import json 
+# #1.
+# with open(r"./SendMessage/SRRD_Code.json","r") as fp: #앞에 세이브에서 저장한 코드값을 찾아옴
+#     ts = json.load(fp)
+# print(ts)
+# print(ts["access_token"])
+
+with open(r"./SendMessage/SD_code.json","r") as fp: #앞에 세이브에서 저장한 코드값을 찾아옴
+    ts = json.load(fp)
+print(ts)
+print(ts["access_token"])
